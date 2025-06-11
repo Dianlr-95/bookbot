a0df8d532cfdace8f7aaee9ccf9424b0d8e93d42
Dianlr-95 <dgalr957@gmail.com>
2025-06-11 18:38:29 +0200

    add get_character_count function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count
+from stats import get_character_count
 
 # Function to get the contents of a book and return it as a string
 def get_book_text(filepath):
@@ -12,5 +13,7 @@ def main():
     book = get_book_text(filepath)
     word_count = get_word_count(book)
     print(f"{word_count} words found in the document")
+    char_count = get_character_count(book)
+    print(char_count)
     return None
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@
 def get_word_count(book):
     split_book = book.split()
     return len(split_book)
+
+# Function to count every instance of every character
+def get_character_count(book):
+    lowercase_book = book.lower()
+    character_count = {}
+    for c in lowercase_book:
+        if c not in character_count:
+            character_count[c] = 1
+        else:
+            character_count[c] += 1
+    return character_count
