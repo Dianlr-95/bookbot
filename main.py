255c600f6c832f37ed48130a4261f5e56ff19e04
Dianlr-95 <dgalr957@gmail.com>
2025-06-11 19:33:17 +0200

    print report in specific format

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from stats import get_word_count
 from stats import get_character_count
+from stats import sort_characters
 
 # Function to get the contents of a book and return it as a string
 def get_book_text(filepath):
+
     book_content = ""
     with open(filepath) as f:
         book_content = f.read()
     return book_content
 
+def print_report(filepath, word_count, char_count):
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}t...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in range(0, len(char_count)):
+        if char_count[i]["char"].isalpha():
+            print(f"{char_count[i]["char"]}: {char_count[i]["num"]}")
+    print("============= END ===============")
+
+    return None
+
 def main():
+
     filepath = "books/frankenstein.txt"
     book = get_book_text(filepath)
     word_count = get_word_count(book)
-    print(f"{word_count} words found in the document")
     char_count = get_character_count(book)
-    print(char_count)
+    sorted_chars = sort_characters(char_count)
+    print_report(filepath, word_count, sorted_chars)
     return None
+
 main()
--- stats.py
+++ stats.py
@@ -1,10 +1,13 @@
 # Function to get book as a string and return word count
 def get_word_count(book):
+
     split_book = book.split()
+
     return len(split_book)
 
 # Function to count every instance of every character
 def get_character_count(book):
+
     lowercase_book = book.lower()
     character_count = {}
     for c in lowercase_book:
@@ -12,4 +15,23 @@ def get_character_count(book):
             character_count[c] = 1
         else:
             character_count[c] += 1
+
     return character_count
+
+def sort_on(dict):
+    return dict["num"]
+
+# Transform a dictionary into a list of dictionaries and sort it
+def sort_characters(dict):
+
+    list = []
+
+    for item in dict:
+        aux = {}
+        aux["char"] = item
+        aux["num"] = dict[item]
+        list.append(aux)
+
+    list.sort(reverse=True, key=sort_on)
+
+    return list
